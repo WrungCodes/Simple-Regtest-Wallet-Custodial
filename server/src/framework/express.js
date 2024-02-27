9990f34fb3410aeeeed4c4b51594dfd279cc04b0
Daniel Fayemi <danieltosinfayemi@gmail.com>
2024-02-27 19:52:29 +0100

    feat: push

--- server/src/framework/express.js
+++ server/src/framework/express.js
@@ -19,7 +19,7 @@ const app = express()
 app.use(cors())
 app.use(function(req, res, next) {
   res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Opener-Policy", "localhost");
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Headers', 'http://localhost:3000');
   next();
