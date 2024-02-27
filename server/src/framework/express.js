1502582434f9ed19546ba39af988a0992875796b
Daniel Fayemi <danieltosinfayemi@gmail.com>
2024-02-27 18:52:16 +0100

    feat: push

--- server/src/framework/express.js
+++ server/src/framework/express.js
@@ -17,6 +17,12 @@ const __dirname = path.dirname(__filename)
 const app = express()
 
 app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
+
 app.use(helmet())
 app.use(
   helmet.contentSecurityPolicy({
