cd2c41f4f34b7394ffc3a79b1784c3eaf7fbd5a8
Daniel Fayemi <danieltosinfayemi@gmail.com>
2024-02-28 19:42:27 +0100

    feat: finis

--- client/src/widgets/BankAccounts.js
+++ client/src/widgets/BankAccounts.js
@@ -6,6 +6,7 @@ import {
 import { usePlaidLink } from "react-plaid-link";
 import { useSelector, useDispatch } from "react-redux";
 import { setBankId } from "../store/reducers";
+import { useNavigate } from "react-router-dom";
 
 const BankAccounts = ({ accounts = [] }) => {
   const [initBankLink] = useInitializeBankLinkMutation();
@@ -14,11 +15,12 @@ const BankAccounts = ({ accounts = [] }) => {
   const [creatingLink, setCreatingLink] = useState(false);
   const { bankId } = useSelector((state) => state.transaction);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [timerId, setTimerId] = useState(null);
 
   const { open, ready } = usePlaidLink({
     token: linkToken,
     onSuccess: async (public_token, metadata) => {
-      // send public_token to server
       try {
         await addPublicToken({ publicToken: public_token });
         setLinkToken("");
@@ -29,21 +31,70 @@ const BankAccounts = ({ accounts = [] }) => {
     onExit: async () => {
       setCreatingLink(false);
     },
+    onEvent: async () => {
+      clearTimeout(timerId);
+    }
   });
+
+  /**
+   * There is a reoccurring bug where the page has to be reloaded to be able to open plaid sdk
+   * becuase the `ready` sometime does not ever become true until page refresh
+   * https://github.com/plaid/react-plaid-link/issues/285
+   * this is a hacky way of working around it
+   * 
+   */
+  const setTimeOutToRetryHandleGenerateLinkToken = () => {
+    const timeout = 4000; // Set timeout as 4 seconds
+
+    // create a timeout
+    const id = setTimeout(async () => {
+      // get the correct url of the page and add a query param
+      const currentUrl = new URL(window.location);
+      currentUrl.searchParams.set('retryHandleGenerateLinkToken', 'true');
+      window.history.pushState({}, '', currentUrl);
+
+      // reload the page
+      window.location.reload();
+    }, timeout);
+    setTimerId(id); // Store the timer ID to manage it later
+  }
+
+  const checkAndRetryHandleGenerateLinkToken = () => {
+    // fetch the url param to see if we should try again
+    const urlParams = new URLSearchParams(window.location.search);
+    const shouldRetryHandleGenerateLinkToken = urlParams.get('retryHandleGenerateLinkToken') === 'true';
+  
+    if (shouldRetryHandleGenerateLinkToken) {
+      handleGenerateLinkToken();
+  
+      // Clear the parameter immediately after checking
+      urlParams.delete('retryHandleGenerateLinkToken');
+
+      // return url to what it was
+      const newUrl = `${window.location.pathname}`;
+      window.history.replaceState({}, '', newUrl);
+    }
+  }
 
   const handleGenerateLinkToken = useCallback(async () => {
     setCreatingLink(true);
+    clearTimeout(timerId);
+
     try {
       const response = await initBankLink();
       setLinkToken(response.data.link);
+
+      setTimeOutToRetryHandleGenerateLinkToken()
     } catch (e) {
     }
-  }, [initBankLink]);
+  }, [initBankLink, navigate]);
 
   useEffect(() => {
-    if (linkToken) {
+    if (ready && linkToken) {
       open();
     }
+
+    checkAndRetryHandleGenerateLinkToken()
   }, [ready, linkToken, open]);
 
   return (
